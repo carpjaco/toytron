703ddf779c257e21825d7312397da9951c01a460
Jake Carpenter <carpjaco@isu.edu>
2017-03-16 19:45:05 -0600

    Refactor AddressPipe

--- src/app/core/address.pipe.ts
+++ src/app/core/address.pipe.ts
@@ -1,23 +1,14 @@
 import { PipeTransform, Pipe } from '@angular/core';
 
 import { Address } from './address.model';
+import { leftPad } from './leftpad';
 
 @Pipe({ name: 'address' })
 export class AddressPipe implements PipeTransform {
   transform(value: Address, ...args: any[]) {
-    const opcode = this.leftPad(value.operator, '0', 2);
-    const operand = this.leftPad(value.operand, '0', 2);
+    const opcode = leftPad(value.operator, 2, '0');
+    const operand = leftPad(value.operand, 2, '0');
 
     return `${opcode}${operand}`;
-  }
-
-  private leftPad(value: any, padChar: string, length: number): string {
-    let str = value.toString();
-
-    while (str.length < length) {
-      str = padChar + str;
-    }
-
-    return String(str).slice(str.length - length);
   }
 }
