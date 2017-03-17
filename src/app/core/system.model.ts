1ceba28c4807ff2d41b4870514c86ecab369553e
Jake Carpenter <carpjaco@isu.edu>
2017-03-17 09:26:54 -0600

    Show data in system status only when initialized

--- src/app/core/system.model.ts
+++ src/app/core/system.model.ts
@@ -25,7 +25,7 @@ export class System {
 
   reset(): void {
     this.screen = null;
-    this.accumulator = 0;
+    this.accumulator = null;
     this.prgCounter = null;
     this.input = null;
     this.status = Status.Uninitialized;
@@ -48,6 +48,7 @@ export class System {
     if (this.status === Status.Uninitialized) {
       this.status = Status.Initialized;
       this.prgCounter = 0;
+      this.accumulator = 0;
 
       for (let i = 0; i < instructions.length; i++) {
         this.memory.set(i, instructions[i]);
